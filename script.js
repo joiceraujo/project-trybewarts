e6d98285178852bf2d3b69c7ffcded740d37509b
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-01 09:37:31 -0300

    finalizado o requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+/* eslint-disable complexity */
+const buttonLogin = document.getElementById('login');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#password');
+function emailValidation(event) {
+  event.preventDefault();
+  /* refencia: https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/ */
+  if (email.value.includes('tryber@teste.com') && senha.value.includes('123456')) {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+buttonLogin.addEventListener('click', emailValidation);
