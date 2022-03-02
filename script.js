0edddf5ab2f952d52b73095c2203cff9743362f9
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-02 12:55:46 -0300

    adiciona estilo

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 const buttonLogin = document.getElementById('login');
 const email = document.querySelector('#email');
 const senha = document.querySelector('#password');
+const textarea = document.getElementById('textarea');
+const theAmount = document.getElementById('theAmount');
 function emailValidation(event) {
   event.preventDefault();
-  /* refencia: https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/ */
+  /* referencia: https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/ */
   if (email.value.includes('tryber@teste.com') && senha.value.includes('123456')) {
     window.alert('Olá, Tryber!');
   } else {
@@ -12,3 +14,11 @@ function emailValidation(event) {
   }
 }
 buttonLogin.addEventListener('click', emailValidation);
+
+theAmount.innerHTML = 500;
+
+function charCount() {
+  const maxLength = textarea.getAttribute('maxlength');
+  const currentLenght = textarea.value.length;
+  theAmount.innerHTML = maxLength - currentLenght;
+}
